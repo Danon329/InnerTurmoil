4d21b7057cdf8ff907fdeb7a9dffe42956802235
Danon329 <daniel.salit@hotmail.com>
2025-06-14 08:42:25 +0200

    PlayerComponent

--- UnseenEmotions/scenes/entities/player/Player.gd
+++ UnseenEmotions/scenes/entities/player/Player.gd
@@ -1,32 +1,7 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed: float = 150.0
-@export var jump: float = -200.0
-
-@onready var sprite_2d: Sprite2D = $Sprite2D
-
-const GRAVITY: float = 400.0
-const MAX_FALL: float = 800.0
-
-var _jumped: bool = false
+@onready var player_component: PlayerComponent = $PlayerComponent
 
 func _physics_process(delta: float) -> void:
-	if !is_on_floor():
-		velocity.y += GRAVITY * delta
-	
-	movement_input()
-	
-	move_and_slide()
-
-func movement_input() -> void:
-	if is_on_floor() and Input.is_action_pressed("jump"):
-		velocity.y = jump
-		_jumped = true
-	if Input.is_action_just_released("jump") and _jumped:
-		velocity.y = 0.0
-		_jumped = false
-	
-	velocity.x = speed * Input.get_axis("left", "right")
-	if not is_equal_approx(velocity.x, 0.0):
-		sprite_2d.flip_h = velocity.x < 0
+	player_component.process_body(delta)
---
+++ UnseenEmotions/scenes/entities/player/PlayerComponent.gd
@@ -0,0 +1,71 @@
+class_name PlayerComponent
+extends Node
+
+enum State {
+	PLAY,
+	STOP
+}
+
+# Variables that are changeable
+@export var player: Player
+@export var sprite: Sprite2D # change for animated sprite
+@export var speed: float = 150.0
+@export var jump: float = -200.0
+
+const GRAVITY: float = 400.0
+const MAX_FALL: float = 800.0
+
+var _jumped: bool = false
+var _current_state: State = State.PLAY
+
+func _unhandled_input(event: InputEvent) -> void:
+	pass
+
+
+func process_body(delta: float) -> void:
+	match _current_state:
+		State.PLAY:
+			# Apply gravity
+			if !player.is_on_floor():
+				player.velocity.y += GRAVITY * delta
+			
+			movement_input()
+			fall_out_of_bounds()
+			player.move_and_slide()
+		State.STOP:
+			pass
+		_:
+			pass
+	
+	
+
+func movement_input() -> void:
+	# Jump Logic
+	if player.is_on_floor() and Input.is_action_pressed("jump"):
+		player.velocity.y = jump
+		_jumped = true
+	if Input.is_action_just_released("jump") and _jumped:
+		player.velocity.y = 0.0
+		_jumped = false
+	
+	# Movement (left, right) Logic
+	player.velocity.x = speed * Input.get_axis("left", "right")
+	if not is_equal_approx(player.velocity.x, 0.0):
+		sprite.flip_h = player.velocity.x < 0
+
+
+func fall_out_of_bounds() -> void:
+	if player.position.y >= MAX_FALL:
+		owner.queue_free()
+
+
+func attack() -> void:
+	pass
+
+
+func take_damage() -> void:
+	pass
+
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	take_damage()
